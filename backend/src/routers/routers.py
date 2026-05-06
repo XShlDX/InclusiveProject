2bd1e1a818e92b0d418fea0abeee751a83b8497a
Shahil <dnshasha2017@gmail.com>
2026-05-06 20:53:22 +0500

    new directory

--- backend/src/routers/routers.py
+++ backend/src/routers/routers.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, Request
 from typing import Annotated
 
-from src.ai import chat
+from src.services.ai import chat
 from src.models.models import add_user_data, get_user_requests
 from src.schemas.schemas import Prompt
 
