8e81ff4840ca12d1cea7d2e1c9c541eccf61ca04
Shahil <dnshasha2017@gmail.com>
2026-05-06 21:27:03 +0500

    Архитектураны өзгертіп дұрыстадым.

--- backend/src/services/config.py
+++ backend/src/services/config.py
@@ -2,5 +2,5 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 FRONTEND_DIR = os.path.join(BASE_DIR, "frontend")
-
-join_path = os.path.join(FRONTEND_DIR, "index.html")
+HTML_FILE_PATH = os.path.join(BASE_DIR, "frontend", "pages", "home", "index.html")
+join_path = os.path.join(HTML_FILE_PATH, "index.html")
