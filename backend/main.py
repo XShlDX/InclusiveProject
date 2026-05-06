3a0690ac71c0e7858df65b556084378163a66a67
Shahil <dnshasha2017@gmail.com>
2026-05-06 13:36:34 +0500

    main

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,21 @@
 from typing import Annotated
+from urllib import response
 
+import uvicorn
 from fastapi import FastAPI, Depends
 from pydantic import BaseModel
-
-from src.ai import chat
+import asyncio
+from ollama import AsyncClient
 
 app = FastAPI()
 #  uvicorn main:app --reload
 history = []
+
+async def chat(role, content):
+    messages = [{'role': role, 'content': content}]
+    client = AsyncClient()
+    response = await client.chat('llama3:latest', messages=messages)
+    return response['message']['content']
 
 class Prompt(BaseModel):
     role: str
@@ -37,3 +45,6 @@ async def get_answer(message: Annotated[Prompt, Depends()]):
 @app.get('/history')
 def get_history():
     return history
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
