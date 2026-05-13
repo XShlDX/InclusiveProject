230e5bdd0eadc0f3077413113d0940c0efda8c9b
Shahil <dnshasha2017@gmail.com>
2026-05-14 00:23:23 +0500

    Update API URL

--- frontend/modules/script.js
+++ frontend/modules/script.js
@@ -1,7 +1,7 @@
 // БҰЛ ФАЙЛДІҢ ІШІНДЕ ЗАПРОСТАРДЫ ҚАБЫЛДАУҒА АРНАЛҒАН ФУНКЦИЯЛАР БОЛАДЫ
 // type="module" болу керек
 
-const API_URL = 'https://inclusive-api.vercel.app/';
+const API_URL = "https://inclusive-api.vercel.app";
 
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 // AI FUNCTIONS — вызов actions делегирован в Chat.js
